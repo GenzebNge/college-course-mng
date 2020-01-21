21946f3aa44ac1e2e30d70c4f56fc51a67c81842
GBB <>
2020-01-21 07:28:49 -0500

    Initial set up of the project

---
+++ com.college-course-mng/src/main/java/com/collegecoursemng/com/collegecoursemng/Application.java
@@ -0,0 +1,13 @@
+package com.collegecoursemng.com.collegecoursemng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
